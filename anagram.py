12fc1c92b30ab9911d624a1d7f1999583f1cbeac
Ivy Xue <ivyxue16@163.com>
2020-11-23 21:06:41 +0800

    Compare each character in two strings, O(n^2)

---
+++ anagram.py
@@ -0,0 +1,38 @@
+'''
+Author: Ivy Xue
+Time: 11/23/2020
+This file solves Anagram Problem.
+Description: Anagram is a word or phrase made by transposing the letters of another word or phrase.
+Input Parameter: two words s1, s2 with the same length and all the letters are lower case.
+The function will return whether the two words are anagram.
+Return: True or False
+'''
+
+def anagram1(s1,s2):
+    '''
+    This function will literate through s1, find out whether each charater in s1 can be found correspondingly in s2.
+    There are nested loops in this algo. 
+    This is O(n^2) algorithm.
+    '''
+    pos1 = 0
+    list2 = list(s2)
+    stillOK = True
+    while pos1 < len(s1) and stillOK :
+        pos2 = 0
+        found = False
+        while pos2 < len(list2) and not found:  
+            if s1[pos1] == list2[pos2]:
+                found = True
+            else:
+                pos2 = pos2 + 1
+        if found :
+            list2[pos2] = None
+        else :
+            stillOK = False
+        pos1 = pos1 + 1
+    return stillOK
+
+
+if __name__ == "__main__":
+    print(anagram1('python','typhon'))
+
