01f3060efa7c762195743db5fca892d7de2c2981
Ivy Xue <ivyxue16@163.com>
2020-11-29 23:34:23 +0800

    Start Efficient Prantheses problem

--- Code/effective_prantheses.py
+++ Code/effective_prantheses.py
@@ -1,3 +1,15 @@
+'''
+Author: Ivy Xue
+Time: 11/29/2020
+This file solves Effective Prantheses Problem.
+Description: Determine whether prantheses appear in pairs and in the right order
+Input Parameter: a string containing a series of brackets
+The function will return whether the string is effective prantheses
+Return: True or False
+'''
+
+
+
 from ADT_Stack import Stack
 
 s = Stack()
