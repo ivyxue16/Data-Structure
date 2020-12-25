d59c0ec4ceed5f7393a4c8faa932e3d862a66f21
Ivy Xue <ivyxue16@163.com>
2020-12-25 14:31:37 +0800

    Add Recursion example: Sierpinski Triangle

--- Code/recursion.py
+++ Code/recursion.py
@@ -131,3 +131,37 @@ def fab(n):
 # print(fab(2)) 1
 # print(fab(5)) 5
 # print(fab(20)) 6765
+
+
+'''
+Recursion example: Sierpinski Triangle
+'''
+from turtle import *
+
+def drawTriangle(points,color,myTurtle):
+    myTurtle.fillcolor(color)
+    myTurtle.up()
+    myTurtle.goto(points[0])
+    myTurtle.down()
+    myTurtle.begin_fill()
+    myTurtle.goto(points[1])
+    myTurtle.goto(points[2])
+    myTurtle.goto(points[0])
+    myTurtle.end_fill()
+
+def getMid(p1,p2): 
+    return ((p1[0]+p2[0])/2, (p1[1]+p2[1])/2)
+
+def sierpinski(points,degree,myTurtle):
+    colormap = ['blue','red','green','white','yellow','violet','orange']
+    drawTriangle(points,colormap[degree],myTurtle)
+    if degree > 0:
+        sierpinski([points[0],getMid(points[0],points[1]),getMid(points[0],points[2])],degree-1,myTurtle)
+        sierpinski([points[1],getMid(points[1],points[0]),getMid(points[1],points[2])],degree-1,myTurtle)
+        sierpinski([points[2],getMid(points[2],points[0]),getMid(points[2],points[1])],degree-1,myTurtle)
+    
+myTurtle = Turtle()
+myWin = myTurtle.getscreen()
+myPoints = [(-300,-150),(0,300),(300,-150)]
+sierpinski(myPoints,2,myTurtle)
+myWin.exitonclick()
