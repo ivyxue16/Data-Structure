639d798f06ac8ec767e0d64fa6bbb9a63286e898
Ivy Xue <ivyxue16@163.com>
2021-01-07 09:36:22 +0800

    Add dynamic programming method

--- Code/coin.py
+++ Code/coin.py
@@ -1,8 +1,9 @@
 from typing import List
 
-### find min number of coins 
+### Find min number of coins 
 
- # 1. slow
+ # 1. recursion --  very slow 
+ # repeated operations
 def rec(change,coins):
     minCoins = change
     if change in coins:
@@ -16,7 +17,8 @@ def rec(change,coins):
 
 # print(rec(43,[1,2,5,10,25])) 
 
-# 2. more efficient 
+# 2. recursion -- more efficient 
+# keep record of calculated number of coins
 def recDC(change:int,coins:List,knownresult:List) -> int:
     '''
     This function aims to find out minimum number of coins 
@@ -37,12 +39,22 @@ def recDC(change:int,coins:List,knownresult:List) -> int:
                 knownresult[change] = minCoins
     return minCoins
 
-# x = 101
-# print(recDC(x,[1,5,10,21,25],[0]*(x+1)))
+# change = 101
+# print(recDC(x,[1,5,10,21,25],[0]*(change+1)))
 
 
 
 
-# 3.
-def hjcdc(change:int,coins:List,knownresult:List) -> int:
-    pass
+# 3. Dynamic Programming -- fast
+# not recursion
+def dpCoins(change:int, coins:list,minCoins:list) -> int:
+    for cent in range(1,change+1):
+        coinCount = cent
+        for i in [c for c in coins if c <= cent]:
+            if minCoins[cent-i] + 1 <= coinCount:
+                coinCount = minCoins[cent-i] + 1
+        minCoins[cent] = coinCount
+    return minCoins[change]
+
+
+# print(dpCoins(63,[1,5,10,21,25],[0] * 64))
