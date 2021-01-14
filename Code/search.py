0495ab94e24c58a8b19a7872fa89929029e1fcae
Ivy Xue <ivyxue16@163.com>
2021-01-14 22:20:16 +0800

    Add random list case

--- Code/search.py
+++ Code/search.py
@@ -1,4 +1,6 @@
 from typing import List
+import numpy as np
+
 
 def sequentialSearch(searchList:List, item):
     pos = 0
@@ -11,8 +13,10 @@ def sequentialSearch(searchList:List, item):
             pos += 1
     return found
 
-testlist = [1,2,32,8,17,19,42,13,0]
 
-# print(sequentialSearch(testlist,42))
+testlist = [1,2,32,8,17,19,42,13,0]
+# testlist = np.random.randint(0,100,10)
+# print(testlist)
+print(sequentialSearch(testlist,42))
 # print(sequentialSearch(testlist,18))
 
