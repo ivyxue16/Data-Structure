94d53a15f6a80241205701c0e82b1b25f30f134d
Ivy Xue <ivyxue16@163.com>
2021-01-17 22:11:21 +0800

    Add bubble sorting code

--- Code/sorting.py
+++ Code/sorting.py
@@ -3,9 +3,14 @@ from typing import List
 def bubbleSort(alist:List):
     for passnum in range(len(alist)-1,0,-1):
         for i in range(passnum):
-            if alist[i] > alist[i+1]:
-                alist[i],alist[i+1] = alist[i+1],alist[i]
+            if alist[i] > alist[i+1]: 
+                # temp = alist[i+1]
+                # alist[i+1] = alist[i]
+                # alist[i] = temp
+                alist[i],alist[i+1] = alist[i+1],alist[i]  
+                
 
 alist = [54,26,93,17,31,44,55,20]
 bubbleSort(alist)
-print(alist)
+# print(alist)
+
