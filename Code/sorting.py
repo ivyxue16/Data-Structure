1a1c85d8038a52dc118d75c4081da0bb2b0dffae
Ivy Xue <ivyxue16@163.com>
2021-01-22 10:25:54 +0800

    Add merge sort

--- Code/sorting.py
+++ Code/sorting.py
@@ -57,8 +57,46 @@ def gapInsertionSort(alist,start,gap):
 
 
 
+
 def mergeSort(alist):
-    pass
+    print('Splitting ',alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2 
+        leftlist = alist[:mid]
+        rightlist = alist[mid:]
+
+        mergeSort(leftlist)
+        mergeSort(rightlist)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(leftlist) and j < len(rightlist):
+            if leftlist[i] < rightlist[j]:
+                alist[k] = leftlist[i]
+                i = i + 1
+            else:
+                alist[k] = rightlist[j]
+                j = j + 1
+            k = k + 1
+        
+        while i < len(leftlist):
+            alist[k] = leftlist[i]
+            i = i + 1
+            k = k + 1
+        
+        while j < len(rightlist):
+            alist[k] = rightlist[j]
+            j = j + 1
+            k = k + 1
+        
+        print('Merging ', alist)
+
+
+
+
+
+
 
 def heapSort(alist):
     pass
@@ -80,3 +118,6 @@ if __name__ == "__main__":
 
     # shellSort(alist)
     # print(alist)
+
+    mergeSort(alist)
+    print(alist)
