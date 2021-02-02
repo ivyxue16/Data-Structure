58e20479a8d1abae40a9f9b546dd0826c7ad8d28
Ivy Xue <ivyxue16@163.com>
2021-02-02 14:54:49 +0800

    Add tree traversals methods

--- Code/binarytree.py
+++ Code/binarytree.py
@@ -114,6 +114,24 @@ def evaluate(parseTree):
         return parseTree.getRootVal()
 
 
+def preorder(tree:BinaryTree):
+    if tree:
+        print(tree.getRootVal())
+        preorder(tree.getLeftChild())
+        preorder(tree.getRightChild())
+        
+def postorder(tree:BinaryTree):
+    if tree != None:
+        postorder(tree.getLeftChild())
+        postorder(tree.getRightChild())
+        print(tree.getRootVal())
+
+def inorder(tree:BinaryTree):
+    if tree != None:
+        inorder(tree.getLeftChild())
+        print(tree.getRootVal())
+        inorder(tree.getRightChild())
+
 
 
 if __name__ == "__main__":
