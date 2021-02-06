48a0cae103962622987598fb43eede2978dbde6b
Ivy Xue <ivyxue16@163.com>
2021-02-06 11:58:53 +0800

    Add weight for each character

--- Code/hash.py
+++ Code/hash.py
@@ -11,5 +11,17 @@ def hash(astring:str, tablesize:int):
     return sum % tablesize
 
 
+def weightHash(astring:str, tablesize:int):
+    '''
+    Hash function realization
+    Convert string to a number in hash function
+    Input: string
+    '''
+    sum = 0
+    for i in range(len(astring)):
+        sum = sum + ord(astring[i]) * (i+1)
+    return sum % tablesize
+
 if __name__ == "__main__":
     print(hash('hello',11))
+    print(weightHash('hello',11))
